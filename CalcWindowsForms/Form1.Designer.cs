429ef6aa8f2c5282213a4e1ecf683c253274cd61
tanas <tanas@FOXIK-PC>
2023-10-10 21:22:13 +0300

    Добавьте файлы проекта.

---
+++ CalcWindowsForms/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CalcWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OneNumber = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TwoNumber = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.select = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OneNumber
+            // 
+            this.OneNumber.AutoSize = true;
+            this.OneNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.OneNumber.Font = new System.Drawing.Font("Book Antiqua", 9F);
+            this.OneNumber.Location = new System.Drawing.Point(88, 51);
+            this.OneNumber.Name = "OneNumber";
+            this.OneNumber.Size = new System.Drawing.Size(132, 16);
+            this.OneNumber.TabIndex = 0;
+            this.OneNumber.Text = "Введите первое число";
+            this.OneNumber.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(91, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox2.Location = new System.Drawing.Point(477, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // TwoNumber
+            // 
+            this.TwoNumber.AutoSize = true;
+            this.TwoNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.TwoNumber.Font = new System.Drawing.Font("Book Antiqua", 9F);
+            this.TwoNumber.Location = new System.Drawing.Point(462, 51);
+            this.TwoNumber.Name = "TwoNumber";
+            this.TwoNumber.Size = new System.Drawing.Size(132, 16);
+            this.TwoNumber.TabIndex = 0;
+            this.TwoNumber.Text = "Введите второе число";
+            this.TwoNumber.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(284, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(382, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(211, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.BackColor = System.Drawing.Color.PaleGreen;
+            this.Result.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Bold);
+            this.Result.Location = new System.Drawing.Point(311, 202);
+            this.Result.MaximumSize = new System.Drawing.Size(100, 100);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(66, 17);
+            this.Result.TabIndex = 0;
+            this.Result.Text = "Результат";
+            this.Result.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(222, 234);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(250, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(294, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // select
+            // 
+            this.select.AutoSize = true;
+            this.select.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.select.Font = new System.Drawing.Font("Book Antiqua", 9F);
+            this.select.Location = new System.Drawing.Point(228, 51);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(228, 16);
+            this.select.TabIndex = 0;
+            this.select.Text = "Выберите арифметическую операцию";
+            this.select.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(884, 661);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TwoNumber);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.OneNumber);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OneNumber;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label TwoNumber;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label select;
+    }
+}
+
---
+++ CalcWindowsForms/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = Convert.ToDouble(textBox1.Text); // присваиваем переменной a значение введеное пользователем в первом текст боксе
+            b = Convert.ToDouble(textBox2.Text); // присваиваем переменной b значение введеное пользователем во втором текст боксе
+
+            switch (comboBox1.Text) // выбор между операциями в comboBox1
+            {
+                case "+":
+                    textBox3.Text = Convert.ToString(a + b); 
+                    break;
+                case "-": 
+                    textBox3.Text = Convert.ToString(a - b);
+                    break;
+                case "*":
+                    textBox3.Text = Convert.ToString(a * b);
+                    break;
+                case "/":
+                    textBox3.Text = Convert.ToString(a / b);
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close(); // закрывает форму при нажатии кнопки "Выход"
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = ""; // очищает текостовые окна
+            textBox3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
